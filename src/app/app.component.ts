c4799e120cf8652d29535dfc7d2654e131213381
Guillermo Castellanos <ragui2001@live.com>
2025-11-10 10:14:21 -0600

    Update WhatsApp button text and improve favicon caching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'hotel-hb-encuestas';
+
+abrirWhatsApp(): void {
+  const numeroEmpresa = '5212713977168';
+  const mensaje = `Hola, Me gustaría platicar con alguien de su equipo para resolver unas dudas.`;
+
+  const mensajeCodificado = encodeURIComponent(mensaje);
+  const urlWhatsApp = `https://wa.me/${numeroEmpresa}?text=${mensajeCodificado}`;
+  
+  window.open(urlWhatsApp, '_blank');
+}
 }
